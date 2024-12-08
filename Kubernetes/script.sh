2200e5788f1730cbb17b86f1280d2f395af8ca4a
amirsadaghdar <77416741+amirsadaghdar@users.noreply.github.com>
2024-12-08 23:19:50 +0000

    lkt

--- Kubernetes/script.sh
+++ Kubernetes/script.sh
@@ -1429,8 +1429,179 @@ kubectl exec -it $PODNAME -- /bin/sh
 printenv | sort
 exit
 
-
 kubectl delete secret app1
 kubectl delete secret app2
 kubectl delete secret app3
 kubectl delete deployment hello-world-secrets-env-from
+
+#################
+### Video 023 ###
+#################
+
+# Pulling a Container from a Private Container Registry in AWS ECR.
+# Pull down a hello-world image. This can be done one of the worker nodes.
+docker image list
+docker image pull psk8s.azurecr.io/hello-app:1.0
+
+# Tagging our image and pushing it to ECR.
+# source_ref: psk8s.azurecr.io/hello-app:1.0    #this is the image pulled from our repo
+# target_ref: 047838238778.dkr.ecr.eu-west-1.amazonaws.com/kube:latest      #this is the image you want to push into your private repository
+docker image tag psk8s.azurecr.io/hello-app:1.0 047838238778.dkr.ecr.eu-west-1.amazonaws.com/kube:latest
+docker push 047838238778.dkr.ecr.eu-west-1.amazonaws.com/kube:latest
+
+# Create our secret that we'll use for our image pull.
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 047838238778.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --region eu-west-1
+
+kubectl create secret docker-registry private-ecr-cred \
+    --docker-server=047838238778.dkr.ecr.eu-west-1.amazonaws.com \
+    --docker-username=AWS \
+    --docker-password=$(aws ecr get-login-password --region eu-west-1) \
+    --docker-email=unused@example.com
+
+kubectl get secret private-ecr-cred -o yaml
+kubectl get secret private-ecr-cred -o jsonpath='{.data.\.dockerconfigjson}' | base64 --decode
+
+# Ensure the image doesn't exist on any of our nodes.
+ctr images list
+sudo ctr --namespace k8s.io image ls "name~=hello-app" -q | sudo xargs ctr --namespace k8s.io image rm
+sudo ctr --namespace k8s.io image ls "name~=hello-app" -q | sudo xargs ctr --namespace k8s.io image rm
+sudo ctr --namespace k8s.io image ls "name~=hello-app" -q | sudo xargs ctr --namespace k8s.io image rm
+
+# Create a deployment using imagePullSecret in the Pod Spec.
+kubectl apply -f deployment-private-registry.yaml
+kubectl describe deployment hello-world-private-registry
+
+# Check out Containers and events section to ensure the container was actually pulled.
+kubectl describe pod hello-world-private-registry-57b79648ff-q9znn
+
+# Clean up and remove the images from the worker node.
+kubectl delete -f deployment-private-registry.yaml
+kubectl delete secret private-reg-cred
+docker image rmi psk8s.azurecr.io/hello-app:1.0
+
+#################
+### Video 024 ###
+#################
+
+#Log into the Control Plane Node to drive these demos.
+ssh aen@c1-cp1
+cd ~/content/course/03/demos
+
+
+#Demo 1 - Creating ConfigMaps
+#Create a PROD ConfigMap
+kubectl create configmap appconfigprod \
+    --from-literal=DATABASE_SERVERNAME=sql.example.local \
+    --from-literal=BACKEND_SERVERNAME=be.example.local
+
+
+#Create a QA ConfigMap
+#We can source our ConfigMap from files or from directories
+#If no key, then the base name of the file
+#Otherwise we can specify a key name to allow for more complex app configs and access to specific configuration elements
+more appconfigqa
+kubectl create configmap appconfigqa \
+    --from-file=appconfigqa
+
+
+#Each creation method yeilded a different structure in the ConfigMap
+kubectl get configmap appconfigprod -o yaml
+kubectl get configmap appconfigqa -o yaml
+
+
+
+
+#Demo 2 - Using ConfigMaps in Pod Configurations
+#First as environment variables
+kubectl apply -f deployment-configmaps-env-prod.yaml
+
+
+#Let's see or configured enviroment variables
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-env-prod | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+
+kubectl exec -it $PODNAME -- /bin/sh 
+printenv | sort
+exit
+
+
+#Second as files
+kubectl apply -f deployment-configmaps-files-qa.yaml
+
+
+#Let's see our configmap exposed as a file using the key as the file name.
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-files-qa | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+
+kubectl exec -it $PODNAME -- /bin/sh 
+ls /etc/appconfig
+cat /etc/appconfig/appconfigqa
+exit
+
+
+#Our ConfigMap key, was the filename we read in, and the values are inside the file.
+#This is how we can read in whole files at a time and present them to the file system with the same name in one ConfigMap
+#So think about using this for daemon configs like nginx, redis...etc.
+kubectl get configmap appconfigqa -o yaml
+
+
+#Updating a configmap, change BACKEND_SERVERNAME to beqa1.example.local
+kubectl edit configmap appconfigqa
+
+
+kubectl exec -it $PODNAME -- /bin/sh 
+watch cat /etc/appconfig/appconfigqa
+exit
+
+
+
+#Cleaning up our demo
+kubectl delete deployment hello-world-configmaps-env-prod
+kubectl delete deployment hello-world-configmaps-files-qa
+kubectl delete configmap appconfigprod
+kubectl delete configmap appconfigqa
+
+
+#Additional examples of using secrets in your Pods
+#I'll leave this up to you to work with...
+
+
+#0 - Reading from a directory, each file's basename will be a key in the ConfigMap...but you can define a key if needed
+kubectl create configmap httpdconfigprod1 --from-file=./configs/
+
+kubectl apply -f deployment-configmaps-directory-qa.yaml
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-directory-qa | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+kubectl exec -it $PODNAME -- /bin/sh 
+ls /etc/httpd
+cat /etc/httpd/httpd.conf
+cat /etc/httpd/ssl.conf
+exit
+
+
+
+#1. Defining a custom key for a file. All configuration will be under that key in the filesystem.
+kubectl create configmap appconfigprod1 --from-file=app1=appconfigprod
+kubectl describe configmap appconfigprod1
+kubectl apply -f deployment-configmaps-files-key-qa.yaml
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-files-key-qa | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+kubectl exec -it $PODNAME -- /bin/sh 
+ls /etc/appconfig
+ls /etc/appconfig/app1
+cat /etc/appconfig/app1
+exit
+
+
+
+#Clean up after our demos
+kubectl delete deployments hello-world-configmaps-files-key-qa
+kubectl delete deployments hello-world-configmaps-directory-qa
+kubectl delete configmap httpdconfigprod1
+kubectl delete configmap appconfigprod1
+
