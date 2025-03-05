9d96498e7024aae4fe6f426d772f3415baac4deb
amirsadaghdar <77416741+amirsadaghdar@users.noreply.github.com>
2025-03-05 23:10:54 +0000

    lkt

--- Kubernetes/script.sh
+++ Kubernetes/script.sh
@@ -3419,46 +3419,34 @@ cat /var/run/secrets/kubernetes.io/serviceaccount/token
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
-
-# You're able to authenticate to the API Server with the user...and retrieve some basic and safe information from the API Server.
+# You're able to authenticate to the API Server with the user and retrieve some basic and safe information from the API Server.
 # See this link for more details on API Discovery Roles: https://kubernetes.io/docs/reference/access-authn-authz/rbac/#discovery-roles.
+# Access to the API fails without the authentication Token.
 curl --cacert $CACERT -X GET https://kubernetes.default.svc/api/
 curl --cacert $CACERT --header "Authorization: Bearer $TOKEN" -X GET https://kubernetes.default.svc/api/
 
-
-#But it doesn't have any permissions to access objects...this user is not authorized to access pods
+# But it doesn't have any permissions to access objects. This user is not authorized to access pods
 curl --cacert $CACERT --header "Authorization: Bearer $TOKEN" -X GET https://kubernetes.default.svc/api/v1/namespaces/default/pods
 exit 
 
-
-#We can also use impersonation to help with our authorization testing
+# We can also use impersonation to help with our authorization testing
 kubectl auth can-i list pods --as=system:serviceaccount:default:mysvcaccount1
 kubectl get pods -v 6 --as=system:serviceaccount:default:mysvcaccount1
-
-
-#Let's leave this deployment running and the service account, we're going to revisit this in our next demo in this module
 
 #################
 ### Video 043 ###
 #################
 
-ssh aen@c1-cp1
-cd ~/content/course/02/demos
-
-
-#1 - Changing authorization for a service account
-#We left off with where serviceaccount didn't have access to the API Server to access Pods
+# Changing authorization for a service account
+# We left off with where serviceaccount didn't have access to the API Server to access Pods
 kubectl auth can-i list pods --as=system:serviceaccount:default:mysvcaccount1
 
-
-#But we can create an RBAC Role and bind that to our service account
-#We define who, can perform what verbs on what resources
+# We can create an RBAC Role and bind that to our service account
+# We define who, can perform what verbs on what resources
 kubectl create role demorole --verb=get,list --resource=pods
 kubectl create rolebinding demorolebinding --role=demorole --serviceaccount=default:mysvcaccount1 
 
-
-#Then the service account can access the API with the 
-#https://kubernetes.io/docs/reference/access-authn-authz/rbac/#service-account-permissions
+# The service account can access the API with the https://kubernetes.io/docs/reference/access-authn-authz/rbac/#service-account-permissions
 kubectl auth can-i list pods --as=system:serviceaccount:default:mysvcaccount1
 kubectl get pods -v 6 --as=system:serviceaccount:default:mysvcaccount1
 
