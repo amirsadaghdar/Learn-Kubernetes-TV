2b5d2bfaab9a9841be031d8f9be666b0ec68d84d
amirsadaghdar <77416741+amirsadaghdar@users.noreply.github.com>
2025-03-14 22:49:52 +0000

    lkt

--- Kubernetes/script.sh
+++ Kubernetes/script.sh
@@ -3612,12 +3612,11 @@ kubectl config unset users.kube@lkt-temp-10.eu-west-1.eksctl.io
 kubectl create clusterrolebinding demouserclusterrolebinding \
   --clusterrole=view --user=demouser1
 
-
 # Create the cluster entry, notice the kubeconfig parameter, this will generate a new file using that name.
 # embed-certs puts the cert data in the kubeconfig entry for this user
 kubectl config set-cluster kubernetes-demo \
   --server=https://B12D3A20D84E8AC258EA2BCCF53854F4.sk1.eu-west-1.eks.amazonaws.com \
-  --certificate-authority=./ca.crt \
+  --certificate-authority=ca.crt \
   --embed-certs=true \
   --kubeconfig=demouser1.conf
 
@@ -3647,67 +3646,210 @@ kubectl config set-context demouser1@kubernetes-demo  \
 # There's a cluster, a user, and a context defined
 kubectl config view --kubeconfig=demouser1.conf
 
-
-#Set the current-context in the kubeconfig file
-#Set the context in the file this is a per kubeconfig file setting
-kubectl config use-context demouser1@kubernetes-demo --kubeconfig=demouser1.conf
+# Set the current-context in the kubeconfig file
+# Set the context in the file this is a per kubeconfig file setting
+kubectl config use-context arn:aws:eks:eu-west-1:047838238778:cluster/lkt-temp-10 --kubeconfig=demouser1.conf
 
 # Using a new kubeconfig file for a new user
 # Create a workload...this is being executed as our normal admin user (kubernetes-admin)!
 # Notice in the output it's loading our ~/.kube/config
 kubectl create deployment nginx --image=nginx -v 6
 
-
-#Test the connection using our demouser kubeconfig file. This user is view only.
-#Notice which kubeconfig file was loaded demouser.conf and it will use the default context in the kubeconfig file
+# Test the connection using our demouser kubeconfig file. This user is view only.
+# Notice which kubeconfig file was loaded demouser1.conf and it will use the default context in the kubeconfig file
 kubectl get pods --kubeconfig=demouser1.conf -v 6
 
-
-#Since this user is bound to the view ClusterRole, it cannot change or delete objects
+# Since this user is bound to the view ClusterRole, it cannot change or delete objects
 kubectl scale deployment nginx --replicas=2 --kubeconfig=demouser.conf
 
-
-#In addition to using --kubeconfig you can set your current kubeconfig with the KUBECONFIG enviroment variable
-#This is useful for switching between kubeconfig files
+# In addition to using --kubeconfig you can set your current kubeconfig with the KUBECONFIG enviroment variable
+# This is useful for switching between kubeconfig files
 export KUBECONFIG=demouser.conf
 kubectl get pods -v 6
 unset KUBECONFIG
 
-
-
-
-# 4 - Let's create a new linux user (-m creates the home director) and then create a new kubeconfig for that user
+# Create a new linux user (-m creates the home director) and then create a new kubeconfig for that user
 sudo useradd -m demouser
 
-
-#Copy the demouser.conf kubeconfig to the home directory of demo user in the default kubeconfig location of .kube/config
+# Copy the demouser.conf kubeconfig to the home directory of demo user in the default kubeconfig location of .kube/config
 sudo mkdir -p /home/demouser/.kube
 sudo cp -i demouser.conf /home/demouser/.kube/config
 sudo chown -R demouser:demouser /home/demouser/.kube/
 
-
-#Switch over to this demo user
+# Switch over to this demo user
 sudo su demouser 
 cd 
 
-
-#Check out the kubeconf file, we don't need --kubeconfig since it's in the default location  ~/.kube/config
+# Check out the kubeconf file, we don't need --kubeconfig since it's in the default location  ~/.kube/config
 kubectl config view
 
-
-#Test access as our new user, check where the config loaded from. Are there pods in the output?
+# Test access as our new user, check where the config loaded from. Are there pods in the output?
 kubectl get pods -v 6
 
+# Change back to our regular user
+exit
 
-#Change back to our regular user
+# Keep demouser.conf around for the demos in the next module on Role Based Access Controls.
+# This user is currently only view, in the next module we'll adjust its RBAC rules to edit the deployment
+# But let's delete that deployment
+kubectl delete deployment nginx
+
+# Delete the clusterrolebinding we created in step 2
+kubectl delete clusterrolebinding demouserclusterrolebinding
+
+
+#################
+### Video 047 ###
+#################
+
+ssh aen@c1-cp1
+cd ~/content/course/04/demos
+
+
+#1 - Role/RoleBinding
+#Create a service account that can read the API
+kubectl create namespace ns1
+kubectl create deployment nginx --image=nginx --namespace ns1
+
+
+#Create a Role, apiGroup is '' since a Pod is in core. Resources (pods) will need to be plural.
+kubectl create role demorole --verb=get,list --resource=pods --namespace ns1 --dry-run=client -o yaml
+kubectl create role demorole --verb=get,list --resource=pods --namespace ns1
+
+
+#Create a RoleBinding, defining which user can access the resources defined in the Role demorole
+#This is the user we created together in the module Managing certicates and kubeconfig Files.
+kubectl create rolebinding demorolebinding --role=demorole --user=demouser --namespace ns1  --dry-run=client -o yaml
+kubectl create rolebinding demorolebinding --role=demorole --user=demouser --namespace ns1
+
+
+#Testing access to resources using can-i and using impersonation...this is a great way to test your rbac configuration
+kubectl auth can-i list pods                        #yes, runs as kubernetes-admin
+kubectl auth can-i list pods        --as=demouser   #no, runs as demouser, but wrong namespace
+kubectl auth can-i list pods        --as=demouser --namespace ns1 #yes, runs as demo user which has rights within the ns1 namespace
+kubectl auth can-i list deployments --as=demouser --namespace ns1 #no, runs as demouser, but user cannot get/list deployments...just pods
+
+
+#Get all the pods in our deployment AS our demouser
+kubectl get pods -l app=nginx --namespace ns1 --as=demouser
+
+
+#Let's try to delete a pod using our user demouser that can only get/list pods in the ns1 namespace
+#The user demouser does not have the permissions to delete a pod, this will fail
+PODNAME=$(kubectl get pods -l app=nginx --as=demouser --namespace ns1 -o jsonpath='{ .items[*].metadata.name }')
+echo $PODNAME
+kubectl delete pod $PODNAME --namespace ns1 --as=demouser
+
+
+#Since this user has only get and list pods, if we try to access another resource with this service account it will fail
+#Let's hold onto this user configuration a little longer and we'll adjust its rights to control that deployment
+kubectl get deployments --namespace ns1 --as=demouser
+
+
+
+
+#2 - ClusterRole/ClusterRoleBinding
+#Create a ClusterRole to access cluster wide/non-namespaced resources
+#Goal is to give demouser access to a cluster-wide resource, nodes.
+kubectl auth can-i list nodes --as=demouser #no
+kubectl get nodes --as=demouser 
+
+
+#To give this user access to the node information, we can use a clusterrole and clusterrolebinding
+kubectl create clusterrole democlusterrole --verb=get,list --resource=nodes
+
+
+#Create a ClusterRoleBinding, allowing the user to read Node information
+kubectl create clusterrolebinding democlusterrolebinding --clusterrole=democlusterrole --user=demouser
+kubectl auth can-i list nodes --as=demouser #yes
+kubectl get nodes --as=demouser 
+
+
+
+
+#3 - Using ClusterRole/RoleBinding - to give a user access to more than one namespace
+#Let's now create a new namespace and a deployment and try to access that deployment with our user...it will fail
+kubectl create namespace ns2    #runs as kubernetes-admin
+kubectl create deployment nginx2 --image=nginx --namespace ns2    #runs as kubernetes-admin
+kubectl get deployment --as=demouser --namespace ns2
+
+
+#Rather than maintain the role in the demorole Role in each namespace, let's delete that RoleBinding and Role from the first demo
+# and use a ClusterRole and RoleBinding for access to resources in  ns1 and ns2 namespaces for our demouser
+kubectl delete rolebinding demorolebinding --namespace ns1
+kubectl delete role demorole --namespace ns1
+
+
+#Create a ClusterRole to be used on both namespaces enabling this user to get/list pods in both namespaces
+kubectl create clusterrole democlusterrolepods --verb=get,list --resource=pods
+
+
+#Create a RoleBinding in each namespace referring to the ClusterRole we just created
+#The name can be the same since the rolebinding is in each namespace
+#This gives our demouser access to get/list pods in each namespace
+kubectl create rolebinding demorolebindingpods --clusterrole=democlusterrolepods  --user=demouser --namespace ns1
+kubectl create rolebinding demorolebindingpods --clusterrole=democlusterrolepods  --user=demouser --namespace ns2
+
+
+#Can we read from both namespaces with our demouser?
+kubectl auth can-i list pods --as=demouser --namespace ns1 #Yes
+kubectl auth can-i list pods --as=demouser --namespace ns2 #Yes
+
+kubectl get pods --as=demouser --namespace ns1
+kubectl get pods --as=demouser --namespace ns2
+
+
+
+
+#4 - Giving a user full access to deployments
+#Let's give demouser access to the deployments in the ns1 namespace
+#The * will give the user full control over the resource, in this case deployments using a Role/RoleBinding
+kubectl create role demoroledeployment --verb=* --resource=deployments --namespace ns1
+
+
+#Now let's create a rolebinding for this user to the newly created role
+kubectl create rolebinding demorolebindingdeployment \
+    --role=demoroledeployment --user=demouser --namespace ns1
+
+
+#Testing out our rights as demouser, rather than impersenation let's switch over to th euser we created in 
+#the module 'Managing Certifcates and kubeconfig Files'
+sudo su demouser
+kubectl get deployment --namespace ns1
+
+
+#Let's get a listing of pods in the namespace. 
+#We still have rights to the pods too because of the ClusterRole/RoleBinding from our third demo in this series of demos
+#Where we defined a ClusterRole and added it to RoleBindings in both namespaces.
+#This is an example of additive rights, this user is in more than one Role/ClusterRole and gets rights to all the defined resources
+kubectl get pods --namespace ns1
+
+
+#demouser now has full control over the deployment so we can update the image
+kubectl describe deployment nginx --namespace ns1 #no image tag specified
+kubectl set image deployment nginx nginx=nginx:1.19.1 --namespace ns1
+kubectl describe deployment nginx --namespace ns1
+
+
+#This user cannot list the deployments in ns2, but has rights to the pods in ns2 due the ClusterRole/Rolebinding demo 2.
+kubectl get deployment --namespace ns2
+kubectl get pods --namespace ns2
+
+
+#log out as demouser
 exit
 
 
 
-#Keep demouser.conf around for the demos in the next module on Role Based Access Controls.
-#This user is currently only view, in the next module we'll adjust its RBAC rules to edit the deployment
-#But let's delete that deployment
-kubectl delete deployment nginx
 
-###Be sure to delete the clusterrolebinding we created in step 2####
-kubectl delete clusterrolebinding demouserclusterrolebinding
+
+#Clean up this demo
+kubectl delete clusterrole democlusterrole
+kubectl delete clusterrole democlusterrolepods
+kubectl delete clusterrolebinding democlusterrolebinding
+kubectl delete namespace ns1
+kubectl delete namespace ns2
+
+
+#Clean up the user we createing the module 3 demos (or not you can keep it around)
+sudo userdel --remove demouser
